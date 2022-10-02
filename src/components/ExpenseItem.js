37da73a1080c9874ef88daea4f21a5abd58a123e
joannewei <joannewei0921@gmail.com>
2022-10-03 00:58:05 +0800

    add: Com expenseDate

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,10 @@
 import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate'
 const ExpenseItem = ({ title, amount, date }) => {
-  const month = date.toLocaleString('en-US', { month:'long'})
-  const year = date.toLocaleString('en-US', { year: 'numeric'})
-  const day = date.toLocaleString('en-US', { day: 'numeric'})
+  
   return (
     <div className='expense-item'>
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+      <ExpenseDate date={date}/>
       <div className="expense-item__description">
         <h2>{title}</h2>
       </div>
