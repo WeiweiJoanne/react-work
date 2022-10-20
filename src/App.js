6a17d39cc74bb1408a93cdb5e07be0b55340572e
joannewei <joannewei0921@gmail.com>
2022-10-21 01:07:46 +0800

    add card & organize file

--- src/App.js
+++ src/App.js
@@ -1,61 +1,8 @@
-import { useState } from 'react';
-import ExpenseItem from './components/ExpenseItem';
+import Expense from './components/Expense/Expense';
+// import Expense from './components/Expense/Expense';
 
 function App() {
-  // const expenses = [
-  //   {
-  //     id: 'e1',
-  //     title: 'Toilet Paper',
-  //     amount: 94.12,
-  //     date: new Date(2020, 7, 14),
-  //   },
-  //   { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-  //   {
-  //     id: 'e3',
-  //     title: 'Car Insurance',
-  //     amount: 294.67,
-  //     date: new Date(2021, 2, 28),
-  //   },
-  //   {
-  //     id: 'e4',
-  //     title: 'New Desk (Wooden)',
-  //     amount: 450,
-  //     date: new Date(2021, 5, 12),
-  //   }
-  // ];
-  
-
-  const [expenses] = useState([
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    }
-  ]);
-  
-  return (
-    <>
-    {
-        expenses.map((v,i)=>{
-          return <ExpenseItem key={v.id} title={v.title} amount={v.amount} date={v.date}/>
-        })
-    }
-    </>
-  )
+  return <Expense />
 }
 
 export default App;
--- src/components/Expense/ExpenseItem.js
+++ src/components/Expense/ExpenseItem.js
@@ -1,9 +1,12 @@
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate'
+
+import Card from '../UI/Card';
+
 const ExpenseItem = ({ title, amount, date }) => {
   
   return (
-    <div className='expense-item'>
+    <Card className='expense-item'>
       <ExpenseDate date={date}/>
       <div className="expense-item__description">
         <h2>{title}</h2>
@@ -11,7 +14,7 @@ const ExpenseItem = ({ title, amount, date }) => {
       <div className="expense-item__price">
         ${amount}
       </div>
-    </div>
+    </Card>
   )
 }
 
