aa74482ad788b9cc243a5ec520247468d1ba2ab3
joannewei <joannewei0921@gmail.com>
2022-11-01 01:00:14 +0800

    add: onClick

--- src/components/Expense/ExpenseItem.js
+++ src/components/Expense/ExpenseItem.js
@@ -1,19 +1,27 @@
 import './ExpenseItem.css'
+import { useState } from 'react'
 import ExpenseDate from './ExpenseDate'
 
 import Card from '../UI/Card';
 
-const ExpenseItem = ({ title, amount, date }) => {
-  
+const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandle = () => {
+    setTitle('update!!!')
+    console.log(title);
+  }
+
   return (
     <Card className='expense-item'>
-      <ExpenseDate date={date}/>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{title}</h2>
       </div>
       <div className="expense-item__price">
-        ${amount}
+        ${props.amount}
       </div>
+      <button onClick={clickHandle}> Change it !!</button>
     </Card>
   )
 }
